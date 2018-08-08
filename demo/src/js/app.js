79105460775b8127fe74fe12ec4ca0fc3c1fdcbc
Olivier Bossel <olivier.bossel@gmail.com>
2018-08-08 17:12:50 +0200

    add demo

---
+++ demo/src/js/app.js
@@ -0,0 +1,5 @@
+import 'babel-polyfill'
+import 'webcomponents.js/webcomponents-lite'
+// import '@webcomponents/webcomponentsjs/bundles/webcomponents-ce'
+// import '@webcomponents/webcomponentsjs/custom-elements-es5-adapter'
+import 'coffeekraken-sugar/js/features/all'
